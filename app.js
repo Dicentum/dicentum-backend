f50afca03f70a1c4df59c8b09856f1f08b136930
Pablo Periañez <pabpercab1@alum.us.es>
2023-11-16 13:13:06 +0100

    feat(public): public
    Creation of new images

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const printer = require('./utils/printer');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth')
+const {MODE} = require("./utils/config");
 
 var app = express();
 
@@ -48,7 +49,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = MODE === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
