6e17eb2cbf160dbfbea08c017ae4f99aef62fb65
Pablo Periañez <pabpercab1@alum.us.es>
2024-05-09 11:53:18 +0200

    fix: pseudorandom number generator

--- controllers/auth/validators/createUniqueUsername.js
+++ controllers/auth/validators/createUniqueUsername.js
@@ -1,4 +1,5 @@
 const User = require('../../../models/users');
+const crypto = require('crypto');
 
 async function createUniqueUsername(name, surname) {
     let username = (name.substring(0, 3) + surname.substring(0, 3)).toLowerCase();
@@ -11,8 +12,8 @@ async function createUniqueUsername(name, surname) {
         counter++;
     }
 
-    let randomNum1 = Math.floor(Math.random() * 10);
-    let randomNum2 = Math.floor(Math.random() * 10);
+    let randomNum1 = crypto.randomBytes(1).readUInt8(0) % 10;
+    let randomNum2 = crypto.randomBytes(1).readUInt8(0) % 10;
 
     username = username + randomNum1.toString() + randomNum2.toString();
 
